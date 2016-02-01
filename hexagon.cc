363aa825cd95296ab7d18250664468fde516df56
Martin Fracker <martin.frackerjr@gmail.com>
2016-01-31 22:02:32 -0600

    Move functions around for readability's sake

--- hexagon.cc
+++ hexagon.cc
@@ -1,17 +1,5 @@
 #include "hexagon.h"
 
-void drawLine(GLint x1, GLint y1, GLint x2, GLint y2) {
-  glBegin(GL_LINES);
-  glVertex2i(x1, y1); // Specify line segment geometry
-  glVertex2i(x2, y2);
-  glEnd();
-}
-
-void maybeColor(GLfloat red, GLfloat green, GLfloat blue, bool pred) {
-  if (pred)
-    glColor3f(red, green, blue);
-}
-
 // draw a w by h octogon with x, y coordinates being the top left
 // corner of an imaginary encompassing square
 // delta is the distance from any vertex to the nearest corner of an
@@ -39,4 +27,16 @@ void drawHexagon(GLint x, GLint y, GLint w, GLint h, GLint delta, bool isRainbow
 
   maybeColor(0.4, 0.2, 0.05, isRainbow); // brown
   drawLine(x + delta, y, x + w - delta, y); 
+}
+
+void maybeColor(GLfloat red, GLfloat green, GLfloat blue, bool pred) {
+  if (pred)
+    glColor3f(red, green, blue);
+}
+
+void drawLine(GLint x1, GLint y1, GLint x2, GLint y2) {
+  glBegin(GL_LINES);
+  glVertex2i(x1, y1); // Specify line segment geometry
+  glVertex2i(x2, y2);
+  glEnd();
 }
